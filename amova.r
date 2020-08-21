3943e1b13bb7b35f7a88906a5ee98c1964cdbd71
Hideo Daikoku <neildaikoku@gmail.com>
2020-08-21 14:22:35 +0900

    updated pca

--- amova.r
+++ amova.r
@@ -1,6 +1,6 @@
 library('textshape')
 library('ade4')
-setwd('~/Documents/Work/Global Jukebox/')
+setwd('~/Documents/Work/sfc-journal-2020/')
 
 indianCanto <- list("samples", "distances", "structures")
 df <- read.csv('./data/samples.csv')
@@ -10,7 +10,7 @@ indianCanto$samples <- column_to_rownames(indianCanto$samples, "canto_coding_id"
 df <- read.csv('./data/indian_no_singles.csv')
 df <- subset(df, select = -c(X, Number.of.Samples, region, division, subregion, area_kingdom, culture, lat, lng))
 df <- column_to_rownames(df, "canto_coding_id")
-indianCanto$distances <- dist(df)
+indianCanto$distances <- dist(df, method="euclidean")
 
 df <- read.csv('./data/structure.csv')
 df = subset(df, select=-c(X))
