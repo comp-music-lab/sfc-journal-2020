60df1eb6c286538f696e613d7cbcb90269bf8f40
Hideo Daikoku <neildaikoku@gmail.com>
2020-10-04 21:14:03 +0900

    latest data

--- amova.r
+++ amova.r
@@ -9,14 +9,14 @@ df <- read.csv('./data/samples.csv')
 indianCanto$samples = subset(df, select = -c(X))
 indianCanto$samples <- column_to_rownames(indianCanto$samples, "canto_coding_id")
 
-df <- read.csv('./data/indian_no_singles.csv')
+df <- read.csv('./data/filtered.csv')
 df <- subset(df, select = -c(X, Number.of.Samples, region, division, subregion, area_kingdom, culture, lat, lng))
 df <- column_to_rownames(df, "canto_coding_id")
 indianCanto$distances <- dist(df, method="euclidean")
 
 df <- read.csv('./data/structure.csv')
 df = subset(df, select=-c(X))
-factors <- df[['region']]
+factors <- df[['language']]
 indianCanto$structures <- data.frame(region = as.factor(factors))
 
 indianCanto$amova <- amova(indianCanto$samples, quasieuclid(indianCanto$distances), indianCanto$structures)
